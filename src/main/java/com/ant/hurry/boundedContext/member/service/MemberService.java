c6f1090343fecf4667cddc3d616d07eaf1bd9186
udonggi <dongki1882@naver.com>
2023-06-21 14:08:22 +0900

    feat: project setting

---
+++ src/main/java/com/ant/hurry/boundedContext/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.ant.hurry.boundedContext.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+}
