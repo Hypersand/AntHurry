54004b3cbf69fde1254025f4f2e28fe1496544c6
Kminwoo97 <kmo971130@gmail.com>
2023-06-21 22:17:01 +0900

    style: 메뉴바에 홈, 로그인, 로그아웃 추가

--- src/main/java/com/ant/hurry/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ant/hurry/boundedContext/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/usr/member")
 public class MemberController {
 
+
     @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String showLogin(){
