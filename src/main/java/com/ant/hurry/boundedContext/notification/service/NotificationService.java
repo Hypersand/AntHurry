54551d774e5c19f6e09f7cba61873681fe1631b3
Hypersand <on8214@gmail.com>
2023-06-22 01:13:04 +0900

    feat: 알림엔티티 생성 기능 추가 #8

--- src/main/java/com/ant/hurry/boundedContext/notification/service/NotificationService.java
+++ src/main/java/com/ant/hurry/boundedContext/notification/service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.ant.hurry.boundedContext.notification.service;
 
+import com.ant.hurry.boundedContext.member.entity.Member;
+import com.ant.hurry.boundedContext.notification.entity.Notification;
 import com.ant.hurry.boundedContext.notification.event.NotifyCancelMessageEvent;
 import com.ant.hurry.boundedContext.notification.event.NotifyEndMessageEvent;
 import com.ant.hurry.boundedContext.notification.event.NotifyNewMessageEvent;
@@ -18,37 +20,54 @@ public class NotificationService {
     private final ApplicationEventPublisher publisher;
 
     //채팅 시작
-    public void notifyNew(String requesterPhoneNumber) {
+    public void notifyNew(Member requester, Member helper) {
+
+        //메시지 제목, 내용 생성
+        String title = "";
+        String content = "";
 
         //알림 엔티티 생성
+        Notification.create(content, requester, helper);
+
 
         //채팅시작알림 이벤트 발생 (title, content 수정 필요)
-        publisher.publishEvent(new NotifyNewMessageEvent(requesterPhoneNumber,"",""));
+        publisher.publishEvent(new NotifyNewMessageEvent(requester.getPhoneNumber(), title, content));
 
     }
 
 
     //거래 완료
-    public void notifyEnd(String requesterPhoneNumber, String helperPhoneNumber) {
+    public void notifyEnd(Member requester, Member helper) {
+
+        //메시지 제목, 내용 생성
+        String title = "";
+        String content = "";
 
         //알림 엔티티 생성
+        Notification.create(content, requester, helper);
 
         //거래 완료 알림 이벤트 발생 (title, content 수정 필요)
-        publisher.publishEvent(new NotifyEndMessageEvent(requesterPhoneNumber, helperPhoneNumber, "", ""));
+        publisher.publishEvent(new NotifyEndMessageEvent(requester.getPhoneNumber(), helper.getPhoneNumber(),
+                title, content));
 
     }
 
 
     //거래 파기
-    public void notifyCancel(String requesterPhoneNumber, String helperPhoneNumber) {
+    public void notifyCancel(Member requester, Member helper) {
+
+        //메시지 제목, 내용 생성
+        String title = "";
+        String content = "";
 
         //알림 엔티티 생성
+        Notification.create(content, requester, helper);
 
         //거래 파기 알림 이벤트 발생 (title, content 수정 필요)
-        publisher.publishEvent(new NotifyCancelMessageEvent(requesterPhoneNumber, helperPhoneNumber, "", ""));
+        publisher.publishEvent(new NotifyCancelMessageEvent(requester.getPhoneNumber(), helper.getPhoneNumber(),
+                title, content));
 
     }
-
 
 
 }
