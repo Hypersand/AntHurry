de8380cbc71a82955d7fee7dc658685e614b19a6
Hypersand <on8214@gmail.com>
2023-06-21 14:24:36 +0900

    feat: 각 계층 기본 구조 생성 #8

---
+++ src/main/java/com/ant/hurry/boundedContext/notification/controller/NotificationController.java
@@ -0,0 +1,14 @@
+package com.ant.hurry.boundedContext.notification.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/notification")
+public class NotificationController {
+
+
+
+}
---
+++ src/main/java/com/ant/hurry/boundedContext/notification/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.ant.hurry.boundedContext.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NotificationService {
+
+}
