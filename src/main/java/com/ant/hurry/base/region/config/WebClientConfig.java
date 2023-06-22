2e471f17a597092e9adb6824bae1e5d7dfcc8986
udonggi <dongki1882@naver.com>
2023-06-22 16:04:53 +0900

    feat: WebClient config 설정

--- src/main/java/com/ant/hurry/base/region/config/WebClientConfig.java
+++ src/main/java/com/ant/hurry/base/region/config/WebClientConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
