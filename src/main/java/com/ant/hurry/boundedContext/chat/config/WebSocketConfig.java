f2ac1aac82440f2ba6d79b14dc2bc98589554532
yurison <yu_ri_son@naver.com>
2023-06-22 13:45:27 +0900

    feat: 채팅 기능 기초적으로 구현(#14)

---
+++ src/main/java/com/ant/hurry/boundedContext/chat/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.ant.hurry.boundedContext.chat.config;
+
+import com.ant.hurry.boundedContext.chat.handler.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/chat").setAllowedOrigins("*");
+    }
+}
