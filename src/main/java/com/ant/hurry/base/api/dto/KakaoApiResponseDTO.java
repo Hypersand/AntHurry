19aa82f1b869db60e6c197d95aee0713dc1b21c8
Jeongmin Lee <dlwogns9807@naver.com>
2023-06-22 17:10:09 +0900

    feat: kakao api 호출 (#12)

---
+++ src/main/java/com/ant/hurry/base/api/dto/KakaoApiResponseDTO.java
@@ -0,0 +1,17 @@
+package com.ant.hurry.base.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KakaoApiResponseDTO {
+
+    private String x;
+    private String y;
+
+}
