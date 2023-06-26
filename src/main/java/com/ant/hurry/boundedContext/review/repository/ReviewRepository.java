b3496ea2241d734e7e7abdf1191a17ae39916c06
Hypersand <on8214@gmail.com>
2023-06-26 17:00:36 +0900

    feat: 후기 목록 페이지 기능 구현 #44

--- src/main/java/com/ant/hurry/boundedContext/review/repository/ReviewRepository.java
+++ src/main/java/com/ant/hurry/boundedContext/review/repository/ReviewRepository.java
@@ -4,10 +4,16 @@ import com.ant.hurry.boundedContext.member.entity.Member;
 import com.ant.hurry.boundedContext.review.entity.Review;
 import com.ant.hurry.boundedContext.tradeStatus.entity.TradeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findByWriterAndTradeStatus(Member writer, TradeStatus tradeStatus);
+
+    @Query("select r from Review r join fetch r.writer w where w = :writer")
+    List<Review> findByWriter(@Param("writer") Member writer);
 }
