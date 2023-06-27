ed29ca150710db493f0eb0511cbb7fb088a158a3
Hypersand <on8214@gmail.com>
2023-06-27 15:44:18 +0900

    feat: 거래상태페이지 로직 구현 #35

--- src/main/java/com/ant/hurry/boundedContext/tradeStatus/controller/TradeStatusController.java
+++ src/main/java/com/ant/hurry/boundedContext/tradeStatus/controller/TradeStatusController.java
@@ -1,12 +1,24 @@
 package com.ant.hurry.boundedContext.tradeStatus.controller;
 
 import com.ant.hurry.base.rq.Rq;
+import com.ant.hurry.base.rsData.RsData;
 import com.ant.hurry.boundedContext.board.service.BoardService;
+import com.ant.hurry.boundedContext.member.entity.Member;
+import com.ant.hurry.boundedContext.member.service.MemberService;
+import com.ant.hurry.boundedContext.tradeStatus.entity.Status;
+import com.ant.hurry.boundedContext.tradeStatus.entity.TradeStatus;
 import com.ant.hurry.boundedContext.tradeStatus.service.TradeStatusService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,6 +27,7 @@ public class TradeStatusController {
 
     private final TradeStatusService tradeStatusService;
     private final BoardService boardService;
+    private final MemberService memberService;
     private final Rq rq;
 
 //    @GetMapping("/create/{id}")
@@ -30,7 +43,14 @@ public class TradeStatusController {
 //    }
 
     @GetMapping("/list")
-    public String showList() {
+    @PreAuthorize("isAuthenticated()")
+    public String showList(@RequestParam(defaultValue = "COMPLETE") String status, @AuthenticationPrincipal User user, Model model) {
+
+        //상대방, 거래상태, 거래시작날짜, 거래후기작성 연결
+        //나의 거래 상태 목록을 모두 띄워야됨.
+        RsData<List<TradeStatus>> rsData = tradeStatusService.findMyTradeStatusList(user.getUsername(), Status.valueOf(status));
+
+        model.addAttribute("tradeStatusList", rsData.getData());
 
         return "tradeStatus/list";
     }
--- src/main/java/com/ant/hurry/boundedContext/tradeStatus/service/TradeStatusService.java
+++ src/main/java/com/ant/hurry/boundedContext/tradeStatus/service/TradeStatusService.java
@@ -1,7 +1,10 @@
 package com.ant.hurry.boundedContext.tradeStatus.service;
 
+import com.ant.hurry.base.rsData.RsData;
 import com.ant.hurry.boundedContext.board.entity.Board;
 import com.ant.hurry.boundedContext.member.entity.Member;
+import com.ant.hurry.boundedContext.member.service.MemberService;
+import com.ant.hurry.boundedContext.review.service.ReviewService;
 import com.ant.hurry.boundedContext.tradeStatus.entity.Status;
 import com.ant.hurry.boundedContext.tradeStatus.entity.TradeStatus;
 import com.ant.hurry.boundedContext.tradeStatus.repository.TradeStatusRepository;
@@ -19,6 +22,7 @@ import static com.ant.hurry.boundedContext.tradeStatus.entity.Status.*;
 public class TradeStatusService {
 
     private final TradeStatusRepository tradeStatusRepository;
+    private final MemberService memberService;
 
     @Transactional
     public TradeStatus create(Board board, Member requester, Member helper) {
@@ -75,5 +79,18 @@ public class TradeStatusService {
 
     public TradeStatus findById(Long id) {
         return tradeStatusRepository.findById(id).orElse(null);
+    }
+
+    public RsData<List<TradeStatus>> findMyTradeStatusList(String username, Status status) {
+
+        Member member = memberService.findByUsername(username).orElse(null);
+
+        if (member == null) {
+            return RsData.of("F_M-1", "존재하지 않는 회원입니다.");
+        }
+
+        List<TradeStatus> tradeStatusList = tradeStatusRepository.findMyTradeStatus(member.getId(), status);
+
+        return RsData.of("S_T-1", "거래상태페이지로 이동합니다.", tradeStatusList);
     }
 }
