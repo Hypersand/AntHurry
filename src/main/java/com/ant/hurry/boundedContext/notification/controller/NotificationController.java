c6e07b902cdea7ece470777f397638cff5419116
Hypersand <on8214@gmail.com>
2023-06-28 21:20:11 +0900

    test: 데이터 및 삭제 테스트 코드 추가 #64"

--- src/main/java/com/ant/hurry/boundedContext/notification/controller/NotificationController.java
+++ src/main/java/com/ant/hurry/boundedContext/notification/controller/NotificationController.java
@@ -12,7 +12,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -40,5 +42,20 @@ public class NotificationController {
 
         return "notification/list";
     }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String delete(@AuthenticationPrincipal User user, @PathVariable Long id) {
+
+        RsData<Notification> rsData = notificationService.delete(id, user.getUsername());
+
+        if (rsData.isFail()) {
+            return rq.historyBack(rsData.getMsg());
+        }
+
+
+        return rq.redirectWithMsg("/notification/list", rsData.getMsg());
+    }
+
 
 }
