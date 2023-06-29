d693e8ccd07f253bead873d6959122cd0fd5290a
Jeongmin Lee <dlwogns9807@naver.com>
2023-06-29 22:59:32 +0900

    fix: 테스트 오류 수정 및 data_h2.sql 수정

--- src/main/resources/data_h2.sql
+++ src/main/resources/data_h2.sql
@@ -12,18 +12,11 @@ INSERT INTO NOTIFICATION (requester_id, helper_id, message, type, created_at) VA
 INSERT INTO NOTIFICATION (requester_id, helper_id, message, type, created_at) VALUES (2, 6, '큰모래님과의 거래가 취소되었습니다.', 'CANCEL', NOW());
 INSERT INTO NOTIFICATION (requester_id, helper_id, message, type, created_at) VALUES (6, 3, '큰모래님과의 거래가 완료되었습니다.', 'END', NOW());
 
-INSERT INTO BOARD (title, content, board_type, x, y, reward_coin, reg_code) VALUES ('제목1', '내용1', '나급해요', 123.127, 37.123, 0, 12345678);
-INSERT INTO BOARD (title, content, board_type, x, y, reward_coin, reg_code) VALUES ('제목2', '내용2', '나급해요', 123.127, 37.123, 0, 12345678);
-INSERT INTO BOARD (title, content, board_type, x, y, reward_coin, reg_code) VALUES ('제목3', '내용3', '나급해요', 123.127, 37.123, 0, 12345678);
-INSERT INTO BOARD (title, content, board_type, x, y, reward_coin, reg_code) VALUES ('제목4', '내용4', '나급해요', 123.127, 37.123, 0, 12345678);
-insert into BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES(1000, 126.732240699017, 37.5110124805355, '나급해요', 'asdffdasd4', '2823710500', '휴지좀 주세요', '오프라인', 1);
-
--- INSERT INTO BOARD (title, content, board_type, trade_type, x, y, reward_coin, reg_code, member_id) VALUES ('제목1', '내용1', '나급해요', '온라인' 127.011390388152, 37.5171300437436, 0, '1165010600', 1);
--- INSERT INTO BOARD (title, content, board_type, trade_type, x, y, reward_coin, reg_code, member_id) VALUES ('제목2', '내용2', '나급해요', '온라인' 127.072375689053, 37.6334020217527, 0, '1135010400', 1);
--- INSERT INTO BOARD (title, content, board_type, trade_type, x, y, reward_coin, reg_code, member_id) VALUES ('제목3', '내용3', '나급해요', '오프라인' 129.135026643506, 35.1657825586672, 0, '2635010500', 1);
--- INSERT INTO BOARD (title, content, board_type, trade_type, x, y, reward_coin, reg_code, member_id) VALUES ('제목4', '내용4', '나급해요', '오프라인' 126.954691116262, 37.4954756157966, 100, '1159010200', 1);
-
-
+INSERT INTO BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES (1000, 126.732240699017, 37.5110124805355, '나급해요', '내용1', '2823710500', '제목1', '오프라인', 1);
+INSERT INTO BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES (1000, 126.732240699017, 37.5110124805355, '나급해요', '내용2', '2823710500', '제목2', '오프라인', 1);
+INSERT INTO BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES (1000, 126.732240699017, 37.5110124805355, '나급해요', '내용3', '2823710500', '제목3', '오프라인', 1);
+INSERT INTO BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES (1000, 126.732240699017, 37.5110124805355, '나급해요', '내용4', '2823710500', '제목4', '오프라인', 1);
+insert into BOARD (reward_coin, x, y, board_type, content, reg_code, title, trade_type, member_id) VALUES(1000, 126.732240699017, 37.5110124805355, '나급해요', '내용5', '2823710500', '휴지좀', '오프라인', 1);
 
 INSERT INTO TRADE_STATUS (board_id, requester_id, helper_id, status) VALUES (1, 1, 2, 'BEFORE');
 INSERT INTO TRADE_STATUS (board_id, requester_id, helper_id, status) VALUES (2, 1, 3, 'CANCELED');
@@ -39,3 +32,4 @@ INSERT INTO REVIEW (content, rating, trade_status_id, writer_id) VALUES ('내용
 INSERT INTO REVIEW (content, rating, trade_status_id, writer_id) VALUES ('내용3', 2.0, 2, 3);
 INSERT INTO REVIEW (content, rating, trade_status_id, writer_id) VALUES ('내용4', 3.0, 3, 2);
 
+INSERT INTO EXCHANGE (member_id, bank_type, account_number, holder_name, money, status) VALUES (1, '국민', '123456789', '홍길동', 1000, 0);
