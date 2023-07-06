8beb26d717a7bd391b43c83b6e40d59f2422b73d
Jeongmin Lee <dlwogns9807@naver.com>
2023-07-06 18:16:59 +0900

    fix: 게시판 상세정보 오류 수정 (#104)

--- src/main/java/com/ant/hurry/boundedContext/tradeStatus/repository/TradeStatusRepository.java
+++ src/main/java/com/ant/hurry/boundedContext/tradeStatus/repository/TradeStatusRepository.java
@@ -28,7 +28,6 @@ public interface TradeStatusRepository extends JpaRepository<TradeStatus, Long> 
     Long countMemberCompleteTradeStatus(@Param("memberId") Long memberId);
 
 
-    @Query("select t from TradeStatus t join fetch t.board b join fetch t.helper h join fetch t.requester r " +
-            "where b.id = :boardId and (h.id = :memberId or r.id = :memberId)")
+    @Query("select t from TradeStatus t join fetch t.board b join fetch t.helper h where b.id = :boardId and h.id = :memberId")
     Optional<TradeStatus> findByHelper(@Param("boardId") Long boardId, @Param("memberId") Long memberId);
 }
