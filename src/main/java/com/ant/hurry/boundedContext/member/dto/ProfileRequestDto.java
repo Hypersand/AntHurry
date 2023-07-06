dea641258bc9a04c50e4a82b9dff46e9688e92fc
Kminwoo97 <kmo971130@gmail.com>
2023-07-06 17:43:26 +0900

    fix: 닉네임 유효성 검사 프론트, 백엔드 통일 (#126)

--- src/main/java/com/ant/hurry/boundedContext/member/dto/ProfileRequestDto.java
+++ src/main/java/com/ant/hurry/boundedContext/member/dto/ProfileRequestDto.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Data
 public class ProfileRequestDto {
 
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,12}$", message = "닉네임은 특수문자를 제외한 4~12자리여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣-_]{4,12}$", message = "닉네임은 특수문자를 제외한 4~12자리여야 합니다.")
     private String nickname;
 
     private String imagePath;
